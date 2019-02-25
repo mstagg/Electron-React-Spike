8ffd6796ebfebc73f21c96787e75da721a117399
Matthew Stagg <matthewmstagg@gmail.com>
2019-02-25 00:38:33 -0500

    update changes, header

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Header = () => (
   <div className="header-banner center-text">
-    <h1 className="header">Welcome to Electron & React</h1>
+    <h1 className="header">Welcome to Electron: React + Redux</h1>
   </div>
 );
 
