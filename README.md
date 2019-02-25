bc43f139e20cee3144cb17217133c41bd3789a3d
Matthew Stagg <matthewmstagg@gmail.com>
2019-02-24 23:22:38 -0500

    readme cleanup and app icon

--- README.md
+++ README.md
@@ -1,20 +1,11 @@
-### 🙋‍♂️ Made by [@thekitze](https://twitter.com/thekitze)  
-
-### Other projects:
-- 🏫 [React Academy](https://reactacademy.io) - Interactive React and GraphQL workshops
-- 💌 [Twizzy](https://twizzy.app) - A standalone app for Twitter DM
-- 💻 [Sizzy](https://sizzy.co) - A tool for testing responsive design on multiple devices at once
-- 🤖 [JSUI](https://github.com/kitze/JSUI) - A powerful UI toolkit for managing JavaScript apps
+### Made by [@mstagg]
 
 ---
 
-# React + Electron = 😍
+# Electron + React + Redux + React-Router Boilerplate
 
-An example of using create-react-app and Electron.
+An example of using Electron with popular React libraries to create a simple electron application. Can be used as boilerplate for a new project, or used as a reference. Builds currently only targets windows, but other OS can be added easily.
 
 ## Scripts
-```yarn start``` will start the Electron app and the React app at the same time.  
-```yarn build``` will build the React app and package it along the Electron app.
-
-## Read more
-You can read more about it in [my Medium article](https://medium.com/@kitze/%EF%B8%8F-from-react-to-an-electron-app-ready-for-production-a0468ecb1da3).
+```npm start``` will start the Electron app and the React app at the same time.  
+```npm run build``` will build the React app and package it along with the Electron app.
--- public/electron.js
+++ public/electron.js
@@ -1,36 +1,37 @@
-const electron = require("electron");
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const electron = require('electron'); // eslint-disable-line import/no-extraneous-dependencies
 
-const path = require("path");
-const isDev = require("electron-is-dev");
+const { app } = electron;
+const { BrowserWindow } = electron;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
-require("update-electron-app")({
-  repo: "kitze/react-electron-example",
-  updateInterval: "1 hour"
+require('update-electron-app')({
+  repo: 'mstagg/Electron-React-Spike',
+  updateInterval: '1 hour',
 });
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow = new BrowserWindow({ width: 900, height: 680, icon: `${__dirname}/app-icon.png` });
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
   );
-  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null)); // eslint-disable-line no-return-assign
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
