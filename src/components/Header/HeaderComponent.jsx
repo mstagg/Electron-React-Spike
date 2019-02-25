ca45497b628351b76f7de7f36cad10b492c1401d
Matthew Stagg <matthewmstagg@gmail.com>
2019-02-24 23:56:38 -0500

    more release testing

--- src/components/Header/HeaderComponent.jsx
+++ src/components/Header/HeaderComponent.jsx
@@ -1,9 +1,11 @@
 import './styles.scss';
 import React from 'react';
 
+const { app } = window.require('electron').remote;
+
 const Header = () => (
   <div className="header-banner center-text">
-    <h1 className="header">Welcome to Electron!</h1>
+    <h1 className="header">{`Welcome to Electron: v${app.getVersion()}`}</h1>
   </div>
 );
 
